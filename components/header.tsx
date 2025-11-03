6f3a657b1e1bd936072c67df8fbd9e3e3aae26dc
Tuguldur <Tuguldur09252@gmail.com>
2025-11-03 16:23:48 +0800

    update-2

--- components/header.tsx
+++ components/header.tsx
@@ -6,19 +6,22 @@ import React from "react";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import { FaSearch } from "react-icons/fa";
 
 const Header = () => {
   return (
-    <header className="flex items-center justify-evenly max-w-screen">
-      <div>
+    <header className="flex items-center justify-evenly max-w-screen h-[100px]">
+      <div className="flex bg-gray-200 max-w-screen w-[97%] rounded-[36px] justify-evenly items-center">
         <div className="flex gap-15px">
-          <Image
-            src="/images/website-logo.png"
-            alt="src.image"
-            width={500}
-            height={500}
-            className="w-[70px] h-[70px]"
-          />
+          <Link href={"/"}>
+            <Image
+              src="/images/website-logo.png"
+              alt="src.image"
+              width={500}
+              height={500}
+              className="w-[70px] h-[70px]"
+            />
+          </Link>
 
           <div className="flex gap-10px">
             <Navbar bg="none" expand="lg">
@@ -28,7 +31,7 @@ const Header = () => {
                   <Nav.Link
                     as={Link}
                     href="/Home"
-                    className="text-white hover:!text-orange-300 duration-300"
+                    className="text-black hover:!text-orange-300 duration-300"
                   >
                     Home
                   </Nav.Link>
@@ -36,7 +39,7 @@ const Header = () => {
                   <Nav.Link
                     as={Link}
                     href="/About"
-                    className="text-white hover:!text-orange-300 duration-300"
+                    className="text-black hover:!text-orange-300 duration-300"
                   >
                     About
                   </Nav.Link>
@@ -44,7 +47,7 @@ const Header = () => {
                   <Nav.Link
                     as={Link}
                     href="/Contact"
-                    className="text-white hover:!text-orange-300 duration-300"
+                    className="text-black hover:!text-orange-300 duration-300"
                   >
                     Contact
                   </Nav.Link>
@@ -52,6 +55,16 @@ const Header = () => {
               </Navbar.Collapse>
             </Navbar>
           </div>
+        </div>
+
+        <div className="flex gap-[8px] outline-1 rounded-[12px] h-[28px] items-center justify-center w-[220px]">
+          <FaSearch size={20} />
+
+          <input
+            type="text"
+            placeholder="Search for pink pussy..."
+            className="rounded-[12px] outline"
+          />
         </div>
       </div>
     </header>
